318ceeacc6211b52c55bbc8330d0ae33e40cfba2
martorres23 <patricia25tg@gmail.com>
2025-06-12 20:05:09 -0500

    Ajustes al prompt del agente Entrevistador

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def obtener_pregunta_por_id(pregunta_id: int) -> Optional[Dict[str, str]]:
 
 @tool
 def psychological_diagnosis(caso_clinico: str) -> str:
-    """Genera un diagnostico en base a un caso clínico"""
+    """Genera un diagnóstico en base a un caso clínico"""
     prompt = f"""Eres un psicólogo clínico especializado en generar diagnósticos. Proporciona:
     1. Diagnóstico preliminar (usa formato DSM-5)
     2. 3 posibles diagnósticos diferenciales
@@ -254,16 +254,20 @@ agent_interviewer = create_react_agent(
 1. Presentate siempre, explica el proceso de una evaluación incicial y pide consentimiento para continuar.
 2. Si el usuario no da su consentimiento, despidete y finaliza la sesión.
 3. Si el usuario da su consentimiento, inicia la entrevista.
-4. Utiliza estrcitamente las preguntas de la base de datos, no inventes las preguntas.
+4. Utiliza estrictamente las preguntas de la base de datos, no inventes las preguntas.
 5. Accede a la base de datos y toma preguntas de la tabla preguntas accediendo a cada una de ellas por medio de un índice que vas a ir aumentando en cada interacción con el usuario(Utiliza SIEMPRE TODAS las preguntas de la base de datos en el orden dispuesto).
 6. Humaniza cada pregunta y siempre sé muy empatico con el usuario(No enumeres las preguntas al paciente ej: pregunta 1: ¿cómo te sientes hoy?).
-7. Si el paciente no cumple con el condicional de la pregunta, pasa a la sigiente pregunta.
+7. Si el paciente no cumple con el condicional de la pregunta, pasa a la sigiente pregunta que sí cumpla el condicional.
 8. Evalúa si la respuesta del usuario es válida y útil (es decir, relevante y no evasiva, como "No sé" o respuestas vacías).
 9. Si la respuesta no es válida, reformulala y muestrala al usuario.
 10. Cuando termines de realizar TODAS las preguntas de la base de datos, crea un caso clínico coherente en base a las respuestas del usuario.
 11. Con el caso clínico genera un diagnostico con la herramienta disponible. Muestra el diagnostico al usuario de una manera humanizada.
 12. En base al caso clínicio y al diagnóstico generado, crea una historia clínica profesional.
 13. Guarda la información del usuario en la base de datos.
+
+REGLAS ADICIONALES:
+- No seas repetitivo en tus respuestas.
+- Sé siempre muy empático y comprensivo, dale palabras de apoyo al paciente("Lo estás haciendo bien", "eres valiente").
 """,
 name="agent_interviewer"
 )
